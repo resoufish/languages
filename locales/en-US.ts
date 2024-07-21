7bfa31843741594d8f78de164d63f2d32604549f
resoufish <not.lovely.tuberculosis@gmail.com>
2024-07-21 16:45:13 +0300

    new locales

--- locales/en-US.ts
+++ locales/en-US.ts
@@ -1,13 +1,105 @@
 import Language from "@bot/structures/Language";
+import { GuildModal } from "@lib/types";
+import { Guild } from "discord.js";
 
 export default class EnglishUs extends Language {
   constructor() {
     super();
 
     this.translations = {
-      COMMAND_ERROR: () => "An error occurred.",
-      COMMAND_PLEASE_WAIT: () => "Please wait...",
-      COMMAND_PING: (data: string) => `WebSocket ping ${data}`,
+      ERROR: (error: any) =>
+        `An error occurred\nError message: \`${error.message}\``,
+      PING: {
+        PLEASE_WAIT: () => "Please wait...",
+        MESSAGE: (ws: number, ping: number, db: number) =>
+          `* WebSocket Latency > \`${ws}ms\`\n` +
+          `* Latency > \`${ping}ms\`\n` +
+          `* Database (Aria) Latency > \`${db}ms\``,
+      },
+      SETTINGS: {
+        GUILD_NOT_FOUND: () =>
+          "Could not find the server in the database, please try again later.",
+        MUSIC_CONFIG_NOT_FOUND: () =>
+          "No music module configuration data found in the database. Please contact support.",
+        UNKNOWN_SUBCOMMAND: () => "Unknown subcommand.",
+      },
+      SETTINGS_EMBED: {
+        EMBED_COLOR_SET: (hex: string) =>
+          `Embed color set to ${hex === "default" ? "default" : `#${hex}`}.`,
+        EMBED_ICON_SET: (type: string) =>
+          `Embed icon type set to ${
+            type === "default" ? "default" : type === "none" ? "none" : "big"
+          }.`,
+      },
+      SETTINGS_LANGUAGE: {
+        LANGUAGE_SET: (locale: string) =>
+          `Bot language set to ${locale === "en-US" ? "English" : locale}.`,
+      },
+      SETTINGS_VIEW: {
+        VIEW_GUILD: (guild: Guild) => `Server settings for ${guild.name}`,
+        VIEW_DESC: (guild: GuildModal) => {
+          const locale = guild.locale === "ru" ? "Русский" : "English";
+          const embedColor =
+            guild.musicConfig?.embedColor === "default"
+              ? "Default"
+              : `#${guild.musicConfig?.embedColor}`;
+          const livePlayerStatus = guild.musicConfig?.livePlayer.enabled
+            ? "Enabled"
+            : "Disabled";
+          const livePlayerChannel =
+            guild.musicConfig?.livePlayer.channelId ?? "Not Set";
+          const searchEngine =
+            guild.musicConfig?.engine.provider ?? "Not Specified";
+          const artWorkType = guild.musicConfig?.artWorkType ?? "Not Specified";
+          const nowPlayingStatus = guild.musicConfig?.nowPlayingPlayer.enabled
+            ? "Enabled"
+            : "Disabled";
+          const analyticsStatus = guild.musicConfig?.analytics.enabled
+            ? "Enabled"
+            : "Disabled";
+          const trackCount = guild.musicConfig?.analytics.tracks.length ?? 0;
+
+          return `
+        # Main Settings >
+        \`\`\`Language: ${locale}\`\`\`
+        
+        # Music Settings >
+        \`\`\`Embed Color: ${embedColor}\n
+        Live Player: ${livePlayerStatus}\n
+        Live Player Channel: ${livePlayerChannel}\n
+        Search Engine: ${searchEngine}\n
+        Artwork Type: ${artWorkType}\n
+        NowPlaying Message: ${nowPlayingStatus}\n
+        Listening Analytics: ${analyticsStatus}\n
+        Analytics Tracks: ${trackCount}\`\`\`
+          `;
+        },
+      },
+      SETTINGS_PLAYER: {
+        MESSAGE_SEND: (send: boolean) =>
+          `Player message send option set to ${
+            send === true ? "Enabled" : "Disabled"
+          }.`,
+        SEARCH_ENGINE_SET: (engine: string) =>
+          `Player search engine set to ${engine}.`,
+      },
+      SETTING_ANALYTICS: {
+        ENABLED_SET: (enable: boolean) =>
+          `Analytics option set to ${
+            enable === true ? "Enabled" : "Disabled"
+          }.`,
+        REMOVE_TRACKS: () => "Analytics data removed.",
+      },
+      SETTINGS_LIVEPLAYER: {
+        ENABLED_SET: (enable: boolean) =>
+          `Live Player option set to ${
+            enable === true ? "Enabled" : "Disabled"
+          }.`,
+        CHANNEL_SET: (channel: string) =>
+          `Live Player channel set to: <#${channel}>.`,
+        CHANNEL_IS_NOT_TEXT: () =>
+          "The selected channel is not a text channel.",
+      },
     };
   }
 }
--- locales/ru.ts
+++ locales/ru.ts
@@ -1,13 +1,97 @@
 import Language from "@bot/structures/Language";
+import { GuildModal } from "@lib/types";
+import { Guild } from "discord.js";
 
 export default class RussianRu extends Language {
   constructor() {
     super();
 
     this.translations = {
-      COMMAND_ERROR: () => "Произошла ошибка.",
-      COMMAND_PLEASE_WAIT: () => "Пожалуйста подождите...",
-      COMMAND_PING: (data: string) => data,
+      ERROR: (error: any) =>
+        `Произашла ошибка\nСообщение ошибки: \`${error.message}\``,
+      PING: {
+        PLEASE_WAIT: () => "Пожалуйста подождите...",
+        MESSAGE: (ws: number, ping: number, db: number) =>
+          `* Задержка WebSocket > \`${ws}ms\`\n` +
+          `* Задрежка > \`${ping}ms\`\n` +
+          `* Задержка Datebase(Aria) > \`${db}ms\``,
+      },
+      SETTINGS: {
+        GUILD_NOT_FOUND: () =>
+          "Не удалось найти сервер в базеданных, попробуйте позже..",
+        MUSIC_CONFIG_NOT_FOUND: () =>
+          "В базеданных не было найдено данных о конфигурации модуля: **музыка**\nОбратитесь на сервер поддержки.",
+        UNKNOWN_SUBCOMMAND: () => "Неизвестная подкоманда.",
+      },
+      SETTINGS_EMBED: {
+        EMBED_COLOR_SET: (hex: string) =>
+          `Цвет эмбеда установлен на #${
+            hex === "default" ? "стандартный" : `#${hex}`
+          }.`,
+        EMBED_ICON_SET: (type: string) =>
+          `Тип иконки плеера изменён на ${
+            type === "default"
+              ? "стандартную"
+              : type === "none"
+              ? "отсутствует"
+              : "большую"
+          }.`,
+      },
+      SETTINGS_LANGUAGE: {
+        LANGUAGE_SET: (locale: string) =>
+          `Язык бота установлен на ${locale === "ru" ? "Русский" : locale}.`,
+      },
+      SETTINGS_VIEW: {
+        VIEW_GUILD: (guild: Guild) => `Настройки сервера ${guild.name}`,
+        VIEW_DESC: (guild: GuildModal) => {
+          const locale = guild.locale === "ru" ? "Русский" : "?";
+          const embedColor =
+            guild.musicConfig?.embedColor === "default"
+              ? "Стандартный"
+              : `#${guild.musicConfig?.embedColor}`;
+          const livePlayerStatus = guild.musicConfig?.livePlayer.enabled
+            ? "Включен"
+            : "Выключен";
+          const livePlayerChannel =
+            guild.musicConfig?.livePlayer.channelId ?? "Не установлен";
+          const searchEngine =
+            guild.musicConfig?.engine.provider ?? "Не указана";
+          const artWorkType = guild.musicConfig?.artWorkType ?? "Не указано";
+          const nowPlayingStatus = guild.musicConfig?.nowPlayingPlayer.enabled
+            ? "Включен"
+            : "Выключен";
+          const analyticsStatus = guild.musicConfig?.analytics.enabled
+            ? "Включена"
+            : "Выключена";
+          const trackCount = guild.musicConfig?.analytics.tracks.length ?? 0;
+
+          return `* Основные настройки\n\`\`\`ts\nЯзык: ${locale}\`\`\`\n* Музыкальные настройки\n\`\`\`ts\nЦвет эмбеда: ${embedColor}\nLive Player: ${livePlayerStatus}\nКанал Live Player'а: ${livePlayerChannel}\nПоисковая система: ${searchEngine}\nТип отображения обложки: ${artWorkType}\nСообщение NowPlaying: ${nowPlayingStatus}\nАналитика прослушивания: ${analyticsStatus}\nТреки аналитики: ${trackCount}\`\`\``;
+        },
+      },
+      SETTINGS_PLAYER: {
+        MESSAGE_SEND: (send: boolean) =>
+          `Опция отправки сообщений плеера установлена на ${
+            send === true ? "Включен." : "Выключен."
+          }.`,
+        SEARCH_ENGINE_SET: (engine: string) =>
+          `Поисковая система плеера установлена на ${engine}.`,
+      },
+      SETTING_ANALYTICS: {
+        ENABLED_SET: (enable: boolean) =>
+          `Опция подсчета аналитики установлена на: ${
+            enable === true ? "Включен." : "Выключен."
+          }.`,
+        REMOVE_TRACKS: () => "Данные аналитики удалины.",
+      },
+      SETTINGS_LIVEPLAYER: {
+        ENABLED_SET: (enable: boolean) =>
+          `Опция Live Player установлена на: ${
+            enable === true ? "Включен." : "Выключен."
+          }.`,
+        CHANNEL_SET: (channel: string) =>
+          `Канал для Live Player'а установлен здесь: <#${channel}>.`,
+        CHANNEL_IS_NOT_TEXT: () => "Выбранный канал не является текстовым.",
+      },
     };
   }
 }
